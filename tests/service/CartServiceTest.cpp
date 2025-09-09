9f5847afd03cb13436bef30e18b939387da0832c
Ravi Nanjundappa <ravi.nanjundappa@thoughtworks.com>
2025-09-09 17:07:49 +0530

    adding unit tests for cartService

---
+++ tests/service/CartServiceTest.cpp
@@ -0,0 +1,86 @@
+#include "../../rest/service/CartService.h"
+#include "../../rest/service/ProductService.h"
+#include "../../rest/service/UserService.h"
+
+#include "../../rest/domain/Cart.h"
+#include "../../rest/domain/Outlet.h"
+#include "../../rest/domain/Product.h"
+#include "../../rest/dto/AddProductRequest.h"
+#include "../../rest/dto/CartProductInfo.h"
+#include <gtest/gtest.h>
+
+#include <vector>
+
+// using namespace detail;
+
+class MockCartServiceTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        GroceryStore store1("Fresh Mart", "Fresh Mart Store", "store1");
+        GroceryProduct product1 = SeedData::createGroceryProduct("Apple", "product1",  store1);
+        product1.setSellingPrice(100.0);
+
+        products.push_back(product1);
+
+        User user1;
+        user1.setUserId("user1");
+        user1.setFirstName("John");
+        user1.setLastName("Doe");
+        users.push_back(user1);
+
+        User user2;
+        user2.setUserId("user2");
+        user2.setFirstName("Rachel");
+        user2.setLastName("Zane");
+        users.push_back(user2);
+        
+        // Initialize UserService and ProductService
+        userService = std::make_unique<UserService>(users);
+        productService = std::make_unique<ProductService>(products);
+        cartService = std::make_unique<CartService>(cartForUsers, *userService, *productService);
+    }
+    void TearDown() override {
+        // Cleanup if necessary
+        products.clear();
+    }
+    std::unique_ptr<UserService> userService;   
+    std::unique_ptr<ProductService> productService;
+    std::unique_ptr<CartService> cartService;
+
+    std::vector<GroceryProduct> products;// = {product};
+    std::vector<User> users;
+
+    std::unordered_map<std::string, Cart> cartForUsers = {
+    {"user1",
+     SeedData::createCartForUser("user1", "John", "Doe", "cart101")},
+    {"user2",
+     SeedData::createCartForUser("user2", "Rachel", "Zane", "cart102")}};
+};
+
+TEST_F(MockCartServiceTest, AddProductToCartForUser_ValidInput) {
+
+    AddProductRequest request;
+    request.setUserId(users[0].getUserId());
+    request.setOutletId("store1");
+    request.setProductId(products[0].getProductId());
+
+    CartProductInfo cartProductInfo = cartService->addProductToCartForUser(request);
+
+    EXPECT_EQ(cartProductInfo.getProduct().getProductId(), "product1");
+    EXPECT_EQ(cartProductInfo.getSellingPrice(), 100.0); // Assuming selling price is 100.0
+    EXPECT_EQ(cartProductInfo.getCart().getProducts().size(), 1);
+}
+
+TEST_F(MockCartServiceTest, GetCartForUser_ValidUser) {
+    Cart cart = cartService->getCartForUser("user1");
+
+    EXPECT_EQ(cart.getCartId(), "cart101");
+    EXPECT_EQ(cart.getUserId(), "user1");
+    EXPECT_EQ(cart.getProducts().size(), 0); // Initially, the cart should be empty
+}
+
+TEST_F(MockCartServiceTest, GetCartForUser_InvalidUser) {
+    EXPECT_THROW({
+        cartService->getCartForUser("invalid_user");
+    }, std::bad_optional_access); // Expecting out_of_range exception for invalid user
+}
