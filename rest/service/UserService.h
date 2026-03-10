49c0f3da2a45e16a1172bcfaed8843458bad4c13
Edward NoaLand <edwardnoaland@gmail.com>
2026-03-10 14:19:18 +0000

    refactor & style: format code in user service and change the signature of code

--- rest/service/UserService.h
+++ rest/service/UserService.h
@@ -1,25 +1,26 @@
 #pragma once
-#include "core/User.h"
-#include "data/SeedData.h"
+
 #include <optional>
 #include <string>
 #include <vector>
 
+#include "core/User.h"
+#include "data/SeedData.h"
+
 class UserService {
+    std::vector<User> users{SeedData::users};
+
 public:
-  UserService(std::vector<User> &usersRef) : users(usersRef) {}
-  UserService() {}
+    UserService() = default;
+    explicit UserService(std::vector<User> users) : users{std::move(users)} {}
 
-  // Return optional to handle "not found" instead of null
-  std::optional<User> fetchUserById(const std::string &userId) {
-    for (const auto &user : users) {
-      if (user.userId == userId) {
-        return user;
-      }
+    // Return optional to handle "not found" instead of null
+    [[nodiscard]] std::optional<User> fetchUserById(const std::string& userId) const {
+        for (const auto& user : users) {
+            if (user.userId == userId) {
+                return user;
+            }
+        }
+        return std::nullopt; // No match found
     }
-    return std::nullopt; // No match found
-  }
-
-private:
-  std::vector<User> users{SeedData::users};
 };
