80c371349ecb72fc56f90db0232af87f305a19a5
Edward NoaLand <edwardnoaland@gmail.com>
2026-03-10 15:48:58 +0000

    style: format code in seed data

--- rest/data/SeedData.h
+++ rest/data/SeedData.h
@@ -10,35 +10,35 @@
 
 class SeedData {
 public:
-  // Static data members
-  static std::unordered_map<std::string, Cart> cartForUsers;
-  static GroceryStore store101;
-  static GroceryStore store102;
-  static User user101;
-  static User user102;
-  static std::vector<std::shared_ptr<Product>> products;
-  static std::vector<User> users;
+    // Static data members
+    static std::unordered_map<std::string, Cart> cartForUsers;
+    static GroceryStore store101;
+    static GroceryStore store102;
+    static User user101;
+    static User user102;
+    static std::vector<std::shared_ptr<Product>> products;
+    static std::vector<User> users;
 
-  // Factory methods
-  static Cart createCartForUser(const std::string &userId,
-                                const std::string &firstName,
-                                const std::string &lastName,
-                                const std::string &cartId);
+    // Factory methods
+    static Cart createCartForUser(const std::string& userId,
+                                  const std::string& firstName,
+                                  const std::string& lastName,
+                                  const std::string& cartId);
 
-  static GroceryStore createStore(const std::string &outletName,
-                                  const std::string &description,
-                                  const std::string &storeId);
+    static GroceryStore createStore(const std::string& outletName,
+                                    const std::string& description,
+                                    const std::string& storeId);
 
-  static User createUser(const std::string &userId,
-                         const std::string &firstName,
-                         const std::string &lastName);
+    static User createUser(const std::string& userId,
+                           const std::string& firstName,
+                           const std::string& lastName);
 
-  static int getRandomNumberUsingNextInt(int min, int max);
+    static int getRandomNumberUsingNextInt(int min, int max);
 
-// private:
-  static std::shared_ptr<GroceryProduct >createGroceryProduct(const std::string &productName,
-                                             const std::string &productId,
-                                             const GroceryStore &store);
+    // private:
+    static std::shared_ptr<GroceryProduct> createGroceryProduct(const std::string& productName,
+                                                                const std::string& productId,
+                                                                const GroceryStore& store);
 };
 
 // Initialize static members
@@ -50,61 +50,68 @@ User SeedData::user101 = SeedData::createUser("user101", "John", "Doe");
 User SeedData::user102 = SeedData::createUser("user102", "Rachel", "Zane");
 
 std::unordered_map<std::string, Cart> SeedData::cartForUsers = {
-    {"user101",
-     SeedData::createCartForUser("user101", "John", "Doe", "cart101")},
-    {"user102",
-     SeedData::createCartForUser("user102", "Rachel", "Zane", "cart102")}};
+    {
+        "user101",
+        SeedData::createCartForUser("user101", "John", "Doe", "cart101")
+    },
+    {
+        "user102",
+        SeedData::createCartForUser("user102", "Rachel", "Zane", "cart102")
+    }
+};
 
 std::vector<std::shared_ptr<Product>> SeedData::products = {
     SeedData::createGroceryProduct("Wheat Bread", "product101",
                                    SeedData::store101),
     SeedData::createGroceryProduct("Spinach", "product102", SeedData::store101),
     SeedData::createGroceryProduct("Crackers", "product103",
-                                   SeedData::store101)};
+                                   SeedData::store101)
+};
 
 std::vector<User> SeedData::users = {SeedData::user101, SeedData::user102};
 
 // Factory method implementations
-Cart SeedData::createCartForUser(const std::string &userId,
-                                 const std::string &firstName,
-                                 const std::string &lastName,
-                                 const std::string &cartId) {
+Cart SeedData::createCartForUser(const std::string& userId,
+                                        const std::string& firstName,
+                                        const std::string& lastName,
+                                        const std::string& cartId) {
     // TODO: remove make_shared later
     return {cartId, userId, std::make_shared<GroceryStore>(store101)};
 }
 
-GroceryStore SeedData::createStore(const std::string &outletName,
-                                   const std::string &description,
-                                   const std::string &storeId) {
-  return {outletName, description, storeId};
+GroceryStore SeedData::createStore(const std::string& outletName,
+                                          const std::string& description,
+                                          const std::string& storeId) {
+    return {outletName, description, storeId};
 }
 
-User SeedData::createUser(const std::string &userId,
-                          const std::string &firstName,
-                          const std::string &lastName) {
-  return {
-      userId, firstName, lastName, firstName + "." + lastName + "@gmail.com",
-      std::to_string(getRandomNumberUsingNextInt(100000000, 900000000))};
+User SeedData::createUser(const std::string& userId,
+                                 const std::string& firstName,
+                                 const std::string& lastName) {
+    return {
+        userId, firstName, lastName, firstName + "." + lastName + "@gmail.com",
+        std::to_string(getRandomNumberUsingNextInt(100000000, 900000000))
+    };
 }
 
 int SeedData::getRandomNumberUsingNextInt(int min, int max) {
-  static std::random_device rd;
-  static std::mt19937 gen(rd());
-  std::uniform_int_distribution<> dist(min, max - 1);
-  return dist(gen);
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(min, max - 1);
+    return dist(gen);
 }
 
-std::shared_ptr<GroceryProduct >SeedData::createGroceryProduct(const std::string &productName,
-                                              const std::string &productId,
-                                              const GroceryStore &store) {
-  auto grocery = std::make_shared<GroceryProduct>();
-  grocery->productId = productId;
-  grocery->productName = productName;
-  grocery->mrp = 10.5f;
-  grocery->weight = 500.0f;
-  grocery->storeId = store.outletId;
-  grocery->threshold = 10;
-  grocery->availableStock = 30;
+std::shared_ptr<GroceryProduct> SeedData::createGroceryProduct(const std::string& productName,
+                                                               const std::string& productId,
+                                                               const GroceryStore& store) {
+    auto grocery = std::make_shared<GroceryProduct>();
+    grocery->productId = productId;
+    grocery->productName = productName;
+    grocery->mrp = 10.5f;
+    grocery->weight = 500.0f;
+    grocery->storeId = store.outletId;
+    grocery->threshold = 10;
+    grocery->availableStock = 30;
 
-  return grocery;
+    return grocery;
 }
